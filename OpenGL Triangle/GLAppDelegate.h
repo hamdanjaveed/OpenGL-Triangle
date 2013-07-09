b9635fb424e42a9a09a42da8b723eb09cf049d8b
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-07-09 15:07:09 -0400

    Initial Commit

---
+++ OpenGL Triangle/GLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GLAppDelegate.h
+//  OpenGL Triangle
+//
+//  Created by Hamdan Javeed on 2013-07-09.
+//  Copyright (c) 2013 Hamdan Javeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OpenGL Triangle/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OpenGL Triangle
+//
+//  Created by Hamdan Javeed on 2013-07-09.
+//  Copyright (c) 2013 Hamdan Javeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GLAppDelegate class]));
+    }
+}
